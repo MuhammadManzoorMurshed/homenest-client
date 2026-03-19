bc38261e631da75137ffa2a62fdc8d4b7284fda7
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-03-20 01:21:36 +0600

    feat: develop signup & signin page UI

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RxEyeOpen } from 'react-icons/rx';
+import { GoEyeClosed } from 'react-icons/go';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+
+    }
+
+    const handleForgotPassword = () => {
+
+    }
+
+    const handleSignInWithGoogle = () => {
+
+    }
+
     return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <h1>I am Sign In Page</h1>
+        <div className='card my-15 bg-teal-100 sm:w-150 md:w-175 sm:mx-auto mx-4 sm:px-6 py-10'>
+            <h1 className='font-fredoka font-semibold text-4xl text-teal-900 text-center mb-10'>Login Now!</h1>
+            <div className='card-body px-4'>
+                <form onSubmit={handleLogin} className="w-full">
+                    <fieldset className="fieldset">
+                        {/* Email */}
+                        <label className="label font-fredoka font-semibold text-lg mt-2" htmlFor='email'>Email</label>
+                        <input type="email" className="input w-full border-0" id='email' name='email' placeholder="@ Email here" />
+
+                        {/* Password */}
+                        <div className='mb-2'>
+                            <label className="label font-fredoka font-semibold text-lg my-2" htmlFor='pass'>Password</label>
+
+                            <div className='relative'>
+                                <input type={showPassword ? "text" : "password"} className="input w-full border-0" id='pass' name='pass' placeholder="@ Password here" />
+                                {
+                                    showPassword ?
+                                        <RxEyeOpen onClick={() => setShowPassword(false)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                        :
+                                        <GoEyeClosed onClick={() => setShowPassword(true)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                }
+                            </div>
+                        </div>
+
+                        {/* Forgot Password */}
+                        <div onClick={handleForgotPassword}><a className="link link-hover">Forgot password?</a></div>
+
+                        {/* Register Button */}
+                        <button className="btn btn-neutral mt-4 font-fredoka font-semibold text-2xl py-6 bg-teal-500 border-0 transform hover:scale-y-105 transition-transform duration-300">Login</button>
+                    </fieldset>
+                </form>
+
+                <div className='text-center font-semibold text-base mt-6'>
+                    <span>Don't have an account?</span>
+                    <span> Click to <Link to={'/authentication/signup'} className='inline-block font-bold text-teal-500 transform hover:text-teal-700 transition-transform duration-300'>register</Link></span>
+                </div>
+
+                <button onClick={() => handleSignInWithGoogle()} className="btn mt-6 mx-auto bg-white text-black border-[#e5e5e5] transform hover:scale-x-95 transition-transform duration-300 w-full">
+                    <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                    Login with Google
+                </button>
+            </div>
         </div>
     );
 };
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RxEyeOpen } from 'react-icons/rx';
+import { GoEyeClosed } from 'react-icons/go';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [errors, setErrors] = useState({firstName: "", lastName: "", email: "", photo: "", password: "", confirtPassword: "", check: ""});
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        setErrors("");
+    }
+
+    const handlePassword = () => {
+
+    }
+
+    const handleConfirmPassword = {
+
+    }
+
     return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <h1>I am Sign Up page</h1>
+        <div className='card my-15 bg-teal-100 sm:w-150 md:w-175 sm:mx-auto mx-4 sm:px-6 py-10'>
+            <h1 className='font-fredoka font-semibold text-4xl text-teal-900 text-center mb-10'>Register Now!</h1>
+            <div className='card-body px-4'>
+                <form onSubmit={handleRegister} className="w-full">
+                    <fieldset className="fieldset">
+                        {/* First Name */}
+                        <label className="label font-fredoka font-semibold text-lg" htmlFor="fname">First Name</label>
+                        <input type="text" className="input w-full border-0" id='fname' name='fname' placeholder="@ First name here" />
+
+                        {/* Last Name */}
+                        <label className="label font-fredoka font-semibold text-lg mt-2" htmlFor='lname'>Last Name</label>
+                        <input type="text" className="input w-full border-0" id='lname' name='lname' placeholder="@ Last name here" />
+
+                        {/* Profile Photo */}
+                        <label className="label font-fredoka font-semibold text-lg mt-2" htmlFor='photo'>Profile Photo</label>
+                        <input type="text" className="input w-full border-0" id='photo' name='photo' placeholder="@ Profile photo URL here" />
+
+                        {/* Email */}
+                        <label className="label font-fredoka font-semibold text-lg mt-2" htmlFor='email'>Email</label>
+                        <input type="email" className="input w-full border-0" id='email' name='email' placeholder="@ Email here" />
+
+                        {/* Password */}
+                        <div className=''>
+                            <label className="label font-fredoka font-semibold text-lg my-2" htmlFor='pass'>Password</label>
+
+                            <div className='relative'>
+                                <input onBlur={handlePassword} type={showPassword ? "password" : "text"} className="input w-full border-0" id='pass' name='pass' placeholder="@ Password here" />
+
+                                {
+                                    showPassword ?
+                                        <RxEyeOpen onClick={() => setShowPassword(false)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                        :
+                                        <GoEyeClosed onClick={() => setShowPassword(true)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                }
+                            </div>
+                            {errors.password && <p className='text-red-500 text-sm'>{errors.password}</p>}
+                        </div>
+
+                        {/*Confirm Password */}
+                        <div>
+                            <label className="label font-fredoka font-semibold text-lg my-2" htmlFor='conpass'>Confirm Password</label>
+
+                            <div className='relative'>
+                                <input onBlur={handleConfirmPassword} type={showConfirmPassword ? "text" : "password"} className="input w-full border-0" id='conpass' name='conpass' placeholder="@ Password here" />
+
+                                {
+                                    showConfirmPassword ?
+                                        <RxEyeOpen onClick={() => setShowConfirmPassword(false)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                        :
+                                        <GoEyeClosed onClick={() => setShowConfirmPassword(true)} className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 cursor-pointer w-4 h-4" />
+                                }
+                            </div>
+                            {errors.confirmPassword && <p className='text-red-500 text-sm'>{errors.confirmPassword}</p>}
+                        </div>
+
+                        <div>
+                            <label className="label mt-4" htmlFor='check'>
+                                <input type="checkbox" className="checkbox" id='check' name='check' />
+                                Accept our terms & conditions
+                            </label>
+                            {errors.check && <p className='text-red-500 text-sm'>{errors.check}</p>}
+                        </div>
+
+                        {/* Register Button */}
+                        <button className="btn btn-neutral mt-4 font-fredoka font-semibold text-2xl py-6 bg-teal-500 border-0 transform hover:scale-y-105 transition-transform duration-300">Register</button>
+                    </fieldset>
+                </form>
+                <div className='text-center font-semibold text-base mt-6'>
+                    <span>Already Registered?</span>
+                    <span> Click to <Link to={'/authentication/signin'} className='font-bold text-teal-500 transform hover:text-teal-700 transition-transform duration-300'>login</Link></span>
+                </div>
+            </div>
         </div>
     );
 };
