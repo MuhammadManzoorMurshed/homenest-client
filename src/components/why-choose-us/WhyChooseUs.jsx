5817a72a62c259d1d4a8a0c6d69c700510002930
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-05-18 02:46:01 +0600

    style: update why choose us design

--- src/components/why-choose-us/WhyChooseUs.jsx
+++ src/components/why-choose-us/WhyChooseUs.jsx
@@ -3,7 +3,7 @@ import ChooseUsCard from './choose-us-card/ChooseUsCard';
 
 const WhyChooseUs = () => {
     return (
-        <section className='max-w-7xl mx-auto my-15 p-5 sm:p-8 md:p-15 lg:p-18 bg-teal-300 rounded-md'>
+        <section className='max-w-7xl mx-auto my-15 sm:my-25 p-5 sm:p-8 md:p-15 lg:p-18 bg-teal-300 rounded-md'>
             <div className='sm:flex sm:justify-between sm:items-start sm:gap-4 md:gap-20'>
                 <div className='flex-1'>
                     <h2 className='font-fredoka font-normal text-[28px] [@media(min-width:32rem)]:text-4xl text-teal-900 text-left'>Why Choose Us</h2>
