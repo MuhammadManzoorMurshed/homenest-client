edf52a68cb4548d2f98ec3cf634790b45231d862
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-05-18 17:27:55 +0600

    feat: remove 2 extra cards from feature section

--- src/components/featured-real-estate/FeaturedRealEstate.jsx
+++ src/components/featured-real-estate/FeaturedRealEstate.jsx
@@ -10,8 +10,8 @@ const FeaturedRealEstate = () => {
             { id: 4, image: '/4.png' },
             { id: 5, image: '/5.png' },
             { id: 6, image: '/6.png' },
-            { id: 7, image: '/7.png' },
-            { id: 8, image: '/8.png' },
+            // { id: 7, image: '/7.png' },
+            // { id: 8, image: '/8.png' },
         ];
 
     return (
