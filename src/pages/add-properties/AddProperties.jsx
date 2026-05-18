8c20d9b2d1e33ed865139e398c97f99004b67934
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-05-18 22:38:43 +0600

    fix: fix implicit type casting issue

--- src/pages/add-properties/AddProperties.jsx
+++ src/pages/add-properties/AddProperties.jsx
@@ -13,7 +13,7 @@ const AddProperties = () => {
         const description = form['description'].value;
         const propertyType = form['property-type'].value;
         const listingPurpose = form['listing-purpose'].value;
-        const price = form['price'].value;
+        const price = Number(form['price'].value);
         const city = form['city'].value;
         const thana = form['thana'].value;
         const area = form['area'].value;
@@ -41,60 +41,61 @@ const AddProperties = () => {
             }
         };
 
-        fetch('http://localhost:3000/add-properties', {
+        fetch('http://localhost:3000/api/v1/add-properties', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(propertyData),
         })
-        .then(res => {
-            if(!res.ok) {
-                throw new Error('Network response was not ok', res.statusText);
-            }
+            .then(res => {
+                console.log("Response: ", res);
+                if (!res.ok) {
+                    throw new Error('Network response was not ok', res.statusText);
+                }
 
-            return res.json();
-        })
-        .then(data => {
-            // console.log('Server response: ', data);
+                return res.json();
+            })
+            .then(data => {
+                // console.log('Server response: ', data);
 
-            if(data.success) {
-                MySwal.fire({
-                    icon: "success",
-                    title: "Property Added",
-                    text: data.message || "Your property has been added successfully!",
-                    confirmButtonText: "OK",
-                    confirmButtonColor: "#38bdf8",
-                })
+                if (data.success) {
+                    MySwal.fire({
+                        icon: "success",
+                        title: "Property Added",
+                        text: data.message || "Your property has been added successfully!",
+                        confirmButtonText: "OK",
+                        confirmButtonColor: "#38bdf8",
+                    })
 
-                form.reset();
-                navigateTo('/my-properties');
-            } else {
+                    form.reset();
+                    navigateTo('/my-properties');
+                } else {
+                    MySwal.fire({
+                        icon: "error",
+                        title: "Error",
+                        text: data.message || "Failed to add property. Please try again.",
+                        confirmButtonText: "OK",
+                        confirmButtonColor: "#ef4444",
+                    })
+                }
+
+            })
+            .catch(error => {
+                // console.error('Error adding property: ', error);
                 MySwal.fire({
                     icon: "error",
                     title: "Error",
-                    text: data.message || "Failed to add property. Please try again.",
+                    text: error.message || "An unexpected error occurred. Please try again.",
                     confirmButtonText: "OK",
                     confirmButtonColor: "#ef4444",
                 })
-            }
-            
-        })
-        .catch(error => {
-            // console.error('Error adding property: ', error);
-            MySwal.fire({
-                icon: "error",
-                title: "Error",
-                text: error.message || "An unexpected error occurred. Please try again.",
-                confirmButtonText: "OK",
-                confirmButtonColor: "#ef4444",
-            })
-        });
+            });
     }
-    
+
     return (
         <div className='mx-auto rounded-lg bg-teal-100 my-15 px-4 py-15 sm:py-20 sm:px-10 sm:w-150 md:w-175'>
-            <form  className='flex flex-col' action="" onSubmit={handleFormSubmit}>
+            <form className='flex flex-col' action="" onSubmit={handleFormSubmit}>
                 <div className='text-center mb-15'>
                     <h1 className='font-fredoka font-semibold text-4xl text-teal-900'>Add New Property</h1>
                     <p className='font-medium text-lg text-teal-500 mt-4'>Complete the form below to publish a new property listing.</p>
@@ -102,7 +103,7 @@ const AddProperties = () => {
 
                 {/* Property Name */}
                 <label className='label font-fredoka font-semibold text-lg mb-2' htmlFor="property-name">Property Name</label>
-                <input className='input w-full border-0' type="text" id="property-name" name="property-name" placeholder='@ e.g. Luxury Apartment in Dhanmondi'/>
+                <input className='input w-full border-0' type="text" id="property-name" name="property-name" placeholder='@ e.g. Luxury Apartment in Dhanmondi' />
 
                 {/* Description */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="description">Description</label>
@@ -136,7 +137,7 @@ const AddProperties = () => {
 
                 {/* Price/Rent */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="price">Price/Rent</label>
-                <input className='input w-full border-0' type="number" id="price" name="price" placeholder='@ Type property price/rent here'/>
+                <input className='input w-full border-0' type="number" id="price" name="price" placeholder='@ Type property price/rent here' />
 
                 {/* Location */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="location">Location</label>
@@ -159,20 +160,20 @@ const AddProperties = () => {
 
                 {/* Images */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="images">Images</label>
-                <input className='input w-full border-0' type="text" name="images" id="images" placeholder='@ Enter image URLs separated by commas'/>
+                <input className='input w-full border-0' type="text" name="images" id="images" placeholder='@ Enter image URLs separated by commas' />
 
                 {/* Name */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="name">Name</label>
-                <input className='input w-full border-0' type="text" id="name" name="name" placeholder='@ Type your name here'/>
+                <input className='input w-full border-0' type="text" id="name" name="name" placeholder='@ Type your name here' />
 
                 {/* Email */}
                 <label className='label font-fredoka font-semibold text-lg mt-3 mb-2' htmlFor="email">Email</label>
-                <input className='input w-full border-0' type="email" id="email" name="email" placeholder='@ Type your email here'/>
+                <input className='input w-full border-0' type="email" id="email" name="email" placeholder='@ Type your email here' />
 
                 {/* Submit Button */}
                 <button className='btn btn-neutral mt-6 font-fredoka font-semibold text-2xl py-6 bg-teal-500 border-0 transform transition-transform hover:scale-y-105 duration-300' type="submit">Add Property</button>
             </form>
-        </div> 
+        </div>
     );
 };
 
