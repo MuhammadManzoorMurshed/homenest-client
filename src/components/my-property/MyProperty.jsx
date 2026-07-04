0954116853ff514832993cf0b2d9d56c540c053f
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-07-04 22:45:27 +0600

    feat (property): add delete property functionality with confirmation model

--- src/components/my-property/MyProperty.jsx
+++ src/components/my-property/MyProperty.jsx
@@ -3,11 +3,62 @@ import { FiMapPin } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import UpdatePropertyModal from '../update-property-modal/UpdatePropertyModal';
 import { useNavigate } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import MySwal from '../../lib/swal';
 
 const MyProperty = ({ myProperty }) => {
+    const queryClient = useQueryClient();
     const modalRef = useRef(null);
     const navigateTo = useNavigate();
-    const {_id, images, propertyName, location, listingPurpose, price } = myProperty || {};
+    const {_id, images, propertyName, location, listingPurpose, price, addedByEmail } = myProperty || {};
+
+    const deleteMutation = useMutation({
+        mutationFn: async () => {
+            const res = await axios.delete(`http://localhost:3000/api/v1/delete-my-property/${_id}`, {
+                params: {
+                    email: addedByEmail
+                }
+            });
+
+            return res.data;
+        },  
+        
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['my-properties']});
+            queryClient.invalidateQueries({queryKey: ['properties']});
+            queryClient.invalidateQueries({ queryKey: ['featured-properties']});
+
+            MySwal.fire({
+                icon: "success",
+                title: "Property Deleted!",
+                text: "Your property has been deleted successfully.",
+                confirmButtonText: "OK",
+                confirmButtonColor: "#0694a2"
+            })
+        }
+    })
+
+    const handleDelete = async () => {
+        if(document.activeElement instanceof HTMLElement) {
+            document.activeElement.blur();
+        }
+
+        const result = await MySwal.fire({
+            icon: "warning",
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            showCancelButton: true,
+            confirmButtonText: "Yes",
+            cancelButtonText: "No",
+            cancelButtonColor: "#0694a2",
+            confirmButtonColor: "#ef4444",
+        });
+
+        if(result.isConfirmed) {
+            deleteMutation.mutate(_id);
+        }
+    }
 
     return (
         <div className="card bg-teal-50 max-w-96 xl:w-96 shadow-sm hover:shadow-lg transition-shadow duration-300 rounded-lg relative group">
@@ -31,7 +82,7 @@ const MyProperty = ({ myProperty }) => {
                     </button>
                 </div>
                 <div className="card-actions justify-between gap-2 items-center mt-4">
-                    <button className="btn text-red-600 font-fredoka text-base bg-teal-50 hover:bg-red-200 "><RiDeleteBin6Line /></button>
+                    <button onClick={handleDelete} className="btn text-red-600 font-fredoka text-base bg-teal-50 hover:bg-red-200 "><RiDeleteBin6Line /></button>
                     <button onClick={() => modalRef.current?.showModal()} className="btn flex-1 font-fredoka text-base bg-teal-500 text-white transition duration-300 hover:scale-x-95">Update</button>
                 </div>
 
